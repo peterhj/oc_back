a10a9a9e7691ac79ddb7c19a6b7ab822a0886e97
Peter Jin <>
2023-12-12 13:39:57 -0500

    Preliminary routing. WIP.

--- src/lib.rs
+++ src/lib.rs
@@ -1,59 +1,153 @@
-extern crate http1;
+extern crate constant_time_eq;
+extern crate rustc_serialize;
 extern crate service_base;
 
+use constant_time_eq::*;
+use rustc_serialize::base64;
 use service_base::prelude::*;
+use service_base::chan::*;
+use service_base::daemon::{protect};
 use service_base::route::*;
 
 use std::convert::{TryInto};
+use std::io::{BufRead, BufReader, Cursor};
 use std::net::{TcpListener, TcpStream};
+use std::sync::{Arc};
+use std::sync::mpsc::{SyncSender, Receiver, sync_channel};
+use std::thread::{sleep, spawn};
+use std::time::{Duration};
 
 pub mod gen_asset;
 pub mod secret_asset;
 pub mod static_asset;
 
 pub fn service_main() -> () {
-  let router = routes();
+  let (front_tx, back_rx) = sync_channel(8);
+  let (back_tx, front_rx) = sync_channel(8);
+  let _ = spawn(move || {
+    'outer: loop {
+      let port_start = 10000;
+      let port_fin = 10009;
+      let mut port = port_start;
+      let stream = loop {
+        match TcpStream::connect(("127.0.0.1", port)) {
+          Err(_) => {}
+          Ok(stream) => break stream
+        }
+        if port >= port_fin {
+          match back_rx.try_recv() {
+            Ok(_) => {
+              let _ = back_tx.send(None);
+              loop {
+                match back_rx.try_recv() {
+                  Ok(_) => {
+                    let _ = back_tx.send(None);
+                  }
+                  _ => {
+                    break;
+                  }
+                }
+              }
+            }
+            //Err(TryRecvError::Empty) => {}
+            _ => {}
+          }
+          sleep(Duration::from_secs(1));
+          continue 'outer;
+        }
+        port += 1;
+      };
+      println!("INFO:   engine: connected on port={}", port);
+      let mut chan = Chan::new(stream);
+      loop {
+        match back_rx.recv() {
+          Ok(req) => {
+            let rep = match chan.query(&Msg::JSO(req)) {
+              Ok(Msg::JSO(rep)) => Some(rep),
+              _ => break
+            };
+            match back_tx.send(rep) {
+              Ok(_) => {}
+              _ => {}
+            }
+          }
+          _ => {
+          }
+        }
+      }
+      println!("INFO:   engine: disconnected");
+    }
+  });
+  let router = Arc::new(routes());
+  let host = "127.0.0.1";
   let port_start = 9000;
   let port_fin = 9009;
   let mut port = port_start;
   let bind = loop {
-    match TcpListener::bind(("127.0.0.1", port)) {
+    match TcpListener::bind((host, port)) {
       Err(_) => {}
       Ok(bind) => break bind
     }
-    if port == port_fin {
+    if port >= port_fin {
       panic!("ERROR:  oc_back::service_main: failed to bind port");
     }
     port += 1;
   };
+  println!("INFO:   listening on {}:{}", host, port);
+  let chroot_dir = "/var/lib/oc_back/new_root";
+  protect(chroot_dir, 297, 297).unwrap();
   let pool = SpawnPool::new(bind);
-  pool.replying(|query| {
+  pool.replying({
     // TODO TODO
-    unimplemented!();
-    /*match query.try_into() {
-      Ok(DecodedMsg::OK) => {
-        Msg::OK
+    let router = router.clone();
+    Arc::new(move |query| {
+      match query {
+        Msg::OKQ => Msg::OKR,
+        Msg::H1Q(req) => {
+          let port = 443;
+          match router.match_(port, &req) {
+            Err(_) | Ok(None) => Msg::Top,
+            Ok(Some(rep)) => Msg::H1P(rep)
+          }
+        }
+        _ => {
+          unimplemented!();
+          //Msg::Bot
+        }
       }
-      Ok(DecodedMsg::HttpReq(req)) => {
-        // TODO
-        unimplemented!();
-      }
-      _ => {
-        unimplemented!();
-      }
-    }*/
+    })
   });
 }
 
+pub fn static_access_tokens() -> Vec<((), String, Box<[u8]>)> {
+  let mut tokens = Vec::new();
+  for line in BufReader::new(Cursor::new(crate::secret_asset::ACCESS_TOKENS)).lines() {
+    let line = line.unwrap();
+    let parts: Vec<_> = line.split(",").collect();
+    tokens.push((
+    (),
+    parts[1].into(),
+    base64::decode_from_str(parts[2]).unwrap().into()));
+  }
+  tokens
+}
+
 pub fn routes() -> Router {
+  let tokens0 = static_access_tokens();
   let mut router = Router::new();
   router.insert_get((), Box::new(move |_, _, _| {
-    unimplemented!();
+    let mut rep = HttpResponse::ok();
+    rep.set_payload_str_with_mime("Hello world!\n", HttpMime::TextHtml);
+    rep.into()
+  }));
+  router.insert_get("about", Box::new(move |_, _, _| {
+    let mut rep = HttpResponse::ok();
+    rep.set_payload_str_with_mime("It&rsquo;s about time.\n", HttpMime::TextHtml);
+    rep.into()
   }));
   router.insert_get(("olympiadchat", "{token:base64}"), Box::new(move |_, args, _| {
     let token = args.get("token")?.as_base64()?;
-    unimplemented!();
-    /*let ident = {
+    let ident = {
       let mut mat_ident = None;
       for &(_, ref ident, ref token0) in tokens0.iter() {
         if constant_time_eq(token0, token) {
@@ -62,12 +156,16 @@ pub fn routes() -> Router {
         }
       }
       mat_ident?
-    };*/
+    };
+    let html = crate::static_asset::CHAT_HTML;
+    let mut rep = HttpResponse::ok();
+    rep.set_payload_str_with_mime(html, HttpMime::TextHtml);
+    rep.into()
     /*
-    let template: _ = crate::static_asset::INDEX_HTML.into();
+    let template: _ = crate::static_asset::CHAT_HTML.into();
     let rendered = template.render(_);
     let mut rep = HttpResponse::ok();
-    rep.set_payload_with_mime(rendered, http1::Mime::TextHtml);
+    rep.set_payload_str_with_mime(rendered, HttpMime::TextHtml);
     rep.into()
     */
   }));
--- src/secret_asset.rs
+++ src/secret_asset.rs
@@ -1,1 +1,1 @@
-pub const STATIC_ACCESS_TOKENS: &'static [u8] = include_bytes!("../secrets/access_tokens.txt");
+pub const ACCESS_TOKENS: &'static [u8] = include_bytes!("../secrets/access_tokens.txt");
