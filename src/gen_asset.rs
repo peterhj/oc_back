1f9ef9e8932f433dbad4c4cd3d4ec251a0f204a3
Peter Jin <>
2024-01-09 12:54:40 -0500

    Fix build.

--- src/gen_asset.rs
+++ src/gen_asset.rs
@@ -1,12 +1,12 @@
 pub static CHAT_MIN_JS: &'static str =
     include_str!(concat!(env!("OUT_DIR"), "/chat.min.js"));
-pub static AUTO_RENDER_MIN_JS_GZ: &'static str =
-    include_str!(concat!(env!("OUT_DIR"), "/auto-render.min.js.gz"));
-pub static KATEX_MIN_JS_GZ: &'static str =
-    include_str!(concat!(env!("OUT_DIR"), "/katex.min.js.gz"));
-pub static CHAT_CSS: &'static str =
-    include_str!(concat!(env!("OUT_DIR"), "/chat.css"));
-pub static KATEX_MIN_CSS_GZ: &'static str =
-    include_str!(concat!(env!("OUT_DIR"), "/katex.min.js.gz"));
-pub static TACHYONS_MIN_CSS_GZ: &'static str =
-    include_str!(concat!(env!("OUT_DIR"), "/tachyons.min.js.gz"));
+pub static AUTO_RENDER_MIN_JS_GZ: &'static [u8] =
+    include_bytes!(concat!(env!("OUT_DIR"), "/auto-render.min.js.gz"));
+pub static KATEX_MIN_JS_GZ: &'static [u8] =
+    include_bytes!(concat!(env!("OUT_DIR"), "/katex.min.js.gz"));
+pub static CHAT_CSS: &'static [u8] =
+    include_bytes!(concat!(env!("OUT_DIR"), "/chat.css"));
+pub static KATEX_MIN_CSS_GZ: &'static [u8] =
+    include_bytes!(concat!(env!("OUT_DIR"), "/katex.min.js.gz"));
+pub static TACHYONS_MIN_CSS_GZ: &'static [u8] =
+    include_bytes!(concat!(env!("OUT_DIR"), "/tachyons.min.js.gz"));
