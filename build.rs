f7ca0c96bb62858f7a53e30e755ef4edab28bee3
Peter Jin <>
2024-01-09 12:42:35 -0500

    Compress static assets during build script.

--- build.rs
+++ build.rs
@@ -1,7 +1,8 @@
+extern crate deflate;
 extern crate jest;
 extern crate time;
 
-use time::{now_utc};
+use time::{get_time};
 
 use std::fs::{OpenOptions};
 use std::io::{BufRead, Write, Cursor};
@@ -9,15 +10,20 @@ use std::path::{PathBuf};
 use std::process::{Command};
 
 fn main() {
-  let t = now_utc();
+  let t = get_time();
   println!("cargo:rerun-if-changed=build.rs");
   println!("cargo:rerun-if-changed=.git/logs/HEAD");
   println!("cargo:rerun-if-changed=../oc_front/.git/logs/HEAD");
   println!("cargo:rerun-if-changed=../oc_front/chat.js");
+  println!("cargo:rerun-if-changed=../oc_front/auto-render.min.js");
+  println!("cargo:rerun-if-changed=../oc_front/katex.min.js");
+  println!("cargo:rerun-if-changed=../oc_front/style.css");
+  println!("cargo:rerun-if-changed=../oc_front/katex.min.css");
+  println!("cargo:rerun-if-changed=../oc_front/tachyons.min.css");
   let out_dir = PathBuf::from(std::env::var("OUT_DIR").unwrap());
   let mut tsp_f = OpenOptions::new().write(true).create(true).truncate(true)
     .open(out_dir.join("timestamp")).unwrap();
-  write!(&mut tsp_f, "{}", t.rfc3339()).unwrap();
+  write!(&mut tsp_f, "{}", t.utc().rfc3339()).unwrap();
   let res = Command::new("git")
     .current_dir(std::env::var("CARGO_MANIFEST_DIR").unwrap())
     .arg("log").arg("-n").arg("1").arg("--format=%H")
@@ -41,5 +47,25 @@ fn main() {
   let src_min = jest::mangle_js_file("../oc_front/chat.js", &["renderMathInElement"]).unwrap();
   let mut src_min_f = OpenOptions::new().write(true).create(true).truncate(true)
     .open(out_dir.join("chat.min.js")).unwrap();
+  write!(&mut src_min_f, "{}", src_min).unwrap();
+  let src_min = deflate::deflate_file_gzip("../oc_front/auto-render.min.js").unwrap();
+  let mut src_min_f = OpenOptions::new().write(true).create(true).truncate(true)
+    .open(out_dir.join("auto-render.min.js.gz")).unwrap();
+  write!(&mut src_min_f, "{}", src_min).unwrap();
+  let src_min = deflate::deflate_file_gzip("../oc_front/katex.min.js").unwrap();
+  let mut src_min_f = OpenOptions::new().write(true).create(true).truncate(true)
+    .open(out_dir.join("katex.min.js.gz")).unwrap();
+  write!(&mut src_min_f, "{}", src_min).unwrap();
+  let src_min = deflate::deflate_file_gzip("../oc_front/style.css").unwrap();
+  let mut src_min_f = OpenOptions::new().write(true).create(true).truncate(true)
+    .open(out_dir.join("style.css.gz")).unwrap();
+  write!(&mut src_min_f, "{}", src_min).unwrap();
+  let src_min = deflate::deflate_file_gzip("../oc_front/katex.min.css").unwrap();
+  let mut src_min_f = OpenOptions::new().write(true).create(true).truncate(true)
+    .open(out_dir.join("katex.min.css.gz")).unwrap();
+  write!(&mut src_min_f, "{}", src_min).unwrap();
+  let src_min = deflate::deflate_file_gzip("../oc_front/tachyons.min.css").unwrap();
+  let mut src_min_f = OpenOptions::new().write(true).create(true).truncate(true)
+    .open(out_dir.join("tachyons.min.css.gz")).unwrap();
   write!(&mut src_min_f, "{}", src_min).unwrap();
 }
